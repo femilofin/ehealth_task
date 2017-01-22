16146398e64e0adf2bb873678e7989600024db33
Oluwafemi <femi@splasherstech.com>
2017-01-22 08:26:51 +0100

    modifications

--- ehealth_task/settings.py
+++ ehealth_task/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ehealth_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ehealth-db',
+        'USER': 'ehealth',
+        'PASSWORD': 'password',
+        'HOST': 'mydbinstance.sample.eu-west-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
